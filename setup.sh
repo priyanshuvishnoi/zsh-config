f332bac5f0f6f37879ef4e4f8b0f6a75642bbcca
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2023-07-12 00:30:46 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Setting Up ZSHENV..."
+echo "export ZDOTDIR=\$HOME/.zsh/" >> $HOME/.zshenv
+
+echo "Getting required packages..."
+mkdir $HOME/.zsh && cd $HOME/.zsh
+
+git clone https://github.com/romkatv/powerlevel10k.git
+git clone https://github.com/marlonrichert/zsh-autocomplete.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+echo "Copying config files..."
+cp .p10k.zsh $HOME/.zsh
+cp .zshrc $HOME/.zsh
+cp .zsh_history $HOME.zsh
+
+echo "Setup Complete"
+echo "Run chsh -s \$(which zsh) to zsh as login shell."
