34d9f80f35443e9a17976f76bf0667ddc049ba0c
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2023-07-30 23:55:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ mkdir $HOME/.zsh
 echo "Copying config files..."
 cp .p10k.zsh $HOME/.zsh/
 cp .zshrc $HOME/.zsh/
-cp .zsh_history $HOME/.zsh/
+# cp .zsh_history $HOME/.zsh/
 
 echo "Getting required packages..."
 cd $HOME/.zsh
